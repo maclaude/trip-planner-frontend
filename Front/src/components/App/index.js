065a6bd1c71c1463296d0f4a1f7c502a9de2ba4a
Gizmo <septier.romain@gmail.com>
2019-03-20 09:20:55 +0100

    navbar component for user

--- Front/src/components/App/index.js
+++ Front/src/components/App/index.js
@@ -10,6 +10,8 @@ import React from 'react';
 import Header from 'src/components/Header';
 import Main from 'src/components/Main';
 import Footer from 'src/components/Footer';
+import NavbarUser from 'src/components/Navbar_user';
+import NewProject from 'src/components/NewProject';
 
 // Style
 import './app.scss';
@@ -19,9 +21,11 @@ import './app.scss';
  */
 const App = () => (
   <div>
-    <Header />
+    {/* <Header />
     <Main />
-    <Footer />
+    <Footer /> */}
+    <NavbarUser />
+    <NewProject />
 
   </div>
 );
--- Front/src/components/Navbar_user/index.js
+++ Front/src/components/Navbar_user/index.js
@@ -7,22 +7,34 @@ import React from 'react';
  * Local import
  */
 // Style
-import './navbar.scss';
+import './navbar_user.scss';
 
 /**
  * Code
  */
-const Navbar = () => (
+const NavbarUser = () => (
   <div id="navbar" className="ui visible inverted left vertical sidebar menu">
     <div id="navbar-username">
       <a className="item"><i className="camera retro icon" />Username</a>
     </div>
     <div id="navbar-main">
       <div id="new-project">
-        <a className="item"><i className="plus icon" /> Crée un nouveau projet </a>
+        <a className="item"><i className="plus icon" /> Crée un nouveau projet</a>
       </div>
       <div id="projects">
-        <a className="item"><i className="angle double right icon" />Mes projets </a>
+        <a className="item">Londres 2019</a>
+      </div>
+      <div id="availability">
+        <a className="item">Définir ses disponibilités</a>
+      </div>
+      <div id="idea">
+        <a className="item">Idées du groupe</a>
+      </div>
+      <div id="summary">
+        <a className="item">Récapitulatif</a>
+      </div>
+      <div id="chat">
+        <a className="item">Espace de discussion</a>
       </div>
     </div>
     <div id="navbar-footer">
@@ -40,4 +52,4 @@ const Navbar = () => (
 /**
  * Export
  */
-export default Navbar;
+export default NavbarUser;
