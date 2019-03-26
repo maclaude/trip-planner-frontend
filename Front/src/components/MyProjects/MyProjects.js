f14f57845d82d1e5e1f56cff51c81b2c59e22d50
Marc-Antoine C <claude.marcantoine2@gmail.com>
2019-03-26 18:02:09 +0100

    Projects data added

--- Front/src/components/MyProjects/MyProjects.js
+++ Front/src/components/MyProjects/MyProjects.js
@@ -1,15 +1,16 @@
 /**
- * NPM IMPORT
+ * NPM import
  */
 import React from 'react';
-import { Card, Image } from 'semantic-ui-react';
-import { Formatizer } from 'formatizer';
-import { NavLink } from 'react-router-dom';
+import { Card } from 'semantic-ui-react';
 
 /**
  * Local import
  */
+// Styles
 import './myprojects.scss';
+// Data
+// import projectsData from 'src/data/projects';
 
 /**
  * Code
@@ -20,61 +21,25 @@ const Projects = () => (
       <h1>Mes projets</h1>
     </div>
     <div id="project-card">
-      <div className="ui link cards">
+      <Card.Group>
         <Card>
-          <Image src="src/assets/images/new-york.jpg" />
           <Card.Content>
             <Card.Header>
               New-York
             </Card.Header>
-            <Card.Meta>
-              Date: 10/07/2019 au 25/07/2019
-            </Card.Meta>
+          </Card.Content>
+          <Card.Content>
             <Card.Description>
               Voyage a New-York entre pote
             </Card.Description>
           </Card.Content>
-          <Card.Content extra>
-            <span>Statut: terminé</span>
-          </Card.Content>
-        </Card>
-        <Card>
-          <Image src="src/assets/images/homepage_img_down.jpg" />
           <Card.Content>
-            <Card.Header>
-            San francisco
-            </Card.Header>
             <Card.Meta>
-            Date: 10/08/2019 au 25/09/2019
+              Date: 10/07/2019 au 25/07/2019
             </Card.Meta>
-            <Card.Description>
-            Voyage a San Francisco
-            </Card.Description>
-          </Card.Content>
-          <Card.Content extra>
-            <span>Statut: Programmé</span>
           </Card.Content>
         </Card>
-        <Card>
-          <Image src="src/assets/images/sweden.jpg" />
-          <Card.Content>
-            <Card.Header>
-              Stockholm
-            </Card.Header>
-            <Card.Meta>
-              Date: 10/12/2019 au 20/12/2019
-            </Card.Meta>
-            <Card.Description>
-              <Formatizer>
-                la suede :hearts:
-              </Formatizer>
-            </Card.Description>
-          </Card.Content>
-          <Card.Content extra>
-            <span>Statut: En cours</span>
-          </Card.Content>
-        </Card>
-      </div>
+      </Card.Group>
     </div>
   </div>
 );
